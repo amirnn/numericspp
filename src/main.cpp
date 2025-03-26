9ed9441c01c41f90e44d55cd103ab0199e811f19
Amir Nourinia <amir.nouri.nia@gmail.com>
2025-03-26 11:04:21 +0100

    init src for playground

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+#include <cstddef>
+#include <stdfloat>
+#include <cstdint>
+#include <cstdlib>
+
+int main(int32_t argc, char const* argv[]){
+    std::float32_t t;
+    int32_t a;
+    uint32_t b;
+    // std::numeric_limits<float>:: ;
+    return EXIT_SUCCESS;
+}
