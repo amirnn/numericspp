c29c913f419d90ce803dbb6a7c6d00c458e5db76
Amir Nourinia <amir.nouri.nia@gmail.com>
2025-03-26 13:58:18 +0100

    lecture 3

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,8 @@
 #include <cstdlib>
 
 int main(int32_t argc, char const* argv[]){
-    std::float32_t t;
-    int32_t a;
-    uint32_t b;
+    std::float16_t f16;
+    std::bfloat16_t bfl16;
     // std::numeric_limits<float>:: ;
     return EXIT_SUCCESS;
 }
